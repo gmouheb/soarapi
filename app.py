5f701fe642972827743f045733e5a52fffa5851e
Mouheb Ghabri <58706486+gmouheb@users.noreply.github.com>
2025-03-01 12:38:54 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+import json
+
+app = Flask(__name__)
+
+# Load JSON data
+with open("data.json", "r") as file:
+    data = json.load(file)
+
+@app.route("/vulnerabilities", methods=["GET"])
+def get_vulnerabilities():
+    return jsonify(data)
+
+@app.route("/vulnerabilities/<ip>", methods=["GET"])
+def get_vulnerabilities_by_ip(ip):
+    filtered_data = [entry for entry in data if entry["Hostname/IP"] == ip]
+    return jsonify(filtered_data if filtered_data else {"message": "No data found for this IP"})
+
+if __name__ == "__main__":
+    app.run(debug=True)
